79727b06456657c6a19c92a317108e6fdf930426
anniechakraborty <the.demented.genius29@gmail.com>
2022-12-01 23:58:54 +0530

    created components cockpit and server-element

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
